e2bd332b44a0916d6c8c395f61e7e90b1c09e927
Evan <evanknight18@gmail.com>
2023-07-08 16:59:22 -0600

    first commit, database logic added

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,32 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  tx.store.put({ content: content });
+  await tx.done;
+  console.log('Content added to the database');
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const allContent = await db.getAll('jate');
+  console.log('Content retrieved from the database');
+  return allContent;
+};
+
+initdb();
