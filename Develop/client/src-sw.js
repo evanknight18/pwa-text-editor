92cf6795f7d1d1dbe9530b3dcd3db763711e8f64
Evan <evanknight18@gmail.com>
2023-07-08 17:15:15 -0600

    service worker asset caching added

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,24 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+  request.destination === 'style' ||
+  request.destination === 'worker' ||
+  request.destination === 'image',
+  assetCache
+);
+
