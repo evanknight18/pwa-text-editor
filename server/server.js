1a676a2f704e76b49cfdf4d111a42bc75012b8e3
Seven Lights <evanknight18@gmail.com>
2023-07-08 20:22:34 -0600

    server update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// update this to point to the build folder in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+} else {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+}
+
 require('./routes/htmlRoutes')(app);
+
+// add the catch all route to serve index.html
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
